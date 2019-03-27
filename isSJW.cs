bd5f02e12f700725c6981ff19965cc75a11e831d
Pant Signal <paulwebberuk@gmail.com>
2019-03-27 11:16:01 -0600

    Create isSJW.cs

---
+++ isSJW.cs
@@ -0,0 +1,12 @@
+void isSJW(){
+    string HairColour;
+    switch (HairColour)
+    {
+        case "NeonRed":
+            Console.Writeline("No need to answer any more questions, definitely SJW.");
+            break;
+        case "NeonPink";
+            Console.Writeline("probably has a blog too, definitely SJW.");
+            break;
+    }   
+}
